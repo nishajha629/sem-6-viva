443a3bb3ceb7dd69a34718c075eb43a48cef291c
Harsh Kulkarni <harsh.kulkarni.42774@gmail.com>
2022-04-23 20:27:53 +0530

    bfs dfs done

--- ai/bfs_dfs.py
+++ ai/bfs_dfs.py
@@ -9,19 +9,22 @@ graph={
 
 def bfs(graph,node):
     print("The Breadth First Search Traversal is")
-    visited=[node]
-    queue=[node]
-    while queue:
+    visited=[node]      #array of all already visited nodes
+    queue=[node]        #queue DS 
+    while queue:        
         print('Visited: ',visited)
-        print('Queue: ',queue,'\n'+'-'*20+'\n')
+        print('Queue: ',queue)
+        print('='*20)
 
-        t=queue.pop(0)
-        for n in graph[t]:
+        element=queue.pop(0)        # dequeue an element
+        for n in graph[element]:
             if n not in visited:
                 visited.append(n)
                 queue.append(n)
+
     print('Visited: ',visited)
-    print('Queue: ',queue,'\n'+'-'*20+'\n')
+    print('Queue: ',queue)
+    print('='*20)
 
 
 
@@ -29,10 +32,12 @@ def dfs(graph,node,visited=[]):
     if node not in visited:
         visited.append(node)
         print('Visited: ',visited)
+        print('-'*10)
         for i in graph[node]:
             dfs(graph,i,visited)
 
 
+print("The Depth First Search Traversal is")
 dfs(graph=graph,node='5')
 print()
 bfs(graph=graph,node='5')
